a80c8d75c3d7039df7f16d4ded426a27cfaba582
Chris Nutter <cdnutter@gmail.com>
2020-11-07 15:11:55 -0800

    im actually excited this works, can't wait to expand

--- gmts.sh
+++ gmts.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env zsh
 
-EXT=$2
+# PDF for now just to test
+
+EXT=$2                          # Extra arguments todo
 IFS=$'\n'
 URL_PARSE=($(wget $1 --no-check-certificate -O - -q  | grep -Eo 'href="[^\"]+.pdf"' | sed 's/href="//g' | sed  's/"//g'))
-# PDF for now just to test
+
 
 [[ ! -d "gmts" ]] && mkdir "gmts"
 
-echo $URL_PARSE
+#echo $URL_PARSE                # Just prints the array
 
 for i in ${URL_PARSE[@]}
 do
