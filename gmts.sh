aa5f95aa8ad17099e3d0f143f84e685545acc577
Chris Nutter <cdnutter@gmail.com>
2020-11-07 15:03:45 -0800

    technically works hahahahaha

--- gmts.sh
+++ gmts.sh
@@ -3,8 +3,13 @@
 EXT=$2
 IFS=$'\n'
 URL_PARSE=($(wget $1 --no-check-certificate -O - -q  | grep -Eo 'href="[^\"]+.pdf"' | sed 's/href="//g' | sed  's/"//g'))
-arr=($URL_PARSE)
+# PDF for now just to test
 
 echo $URL_PARSE
 
-#[[ ! -d "gmts" ]] && mkdir "gmts"
+for i in ${URL_PARSE[@]}
+do
+    $(wget "$i" -P gmts/) 
+done
+
+[[ ! -d "gmts" ]] && mkdir "gmts"
