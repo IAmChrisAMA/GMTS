ab37f287ca7602cdf5639eafcd0db76dafab1bc9
Chris Nutter <cdnutter@gmail.com>
2020-11-07 22:54:52 -0800

    working working working

--- gmts.sh
+++ gmts.sh
@@ -1,18 +1,27 @@
 #! /usr/bin/env zsh
 
-# PDF for now just to test
+[[ ! $1 ]] && echo "Invalid argument\n\t./gmts [EXT] [URL]"; exit
 
-EXT=$2                          # Extra arguments todo
 IFS=$'\n'
-URL_PARSE=($(wget $1 --no-check-certificate -O - -q  | grep -Eo 'href="[^\"]+.pdf"' | sed 's/href="//g' | sed  's/"//g'))
+EXT_PARSE= ($(wget $2 --no-check-certificate -O - -q | grep -Eo 'href="[^\"]+.pdf"' | sed 's/href="//g' | sed 's/"//g'))
+HTML_PARSE=($(wget $2 --no-check-certificate -O - -q | grep -Eo 'href="[^\"]+.html' | sed 's/href="//g' | sed 's/"//g'))
+ARG=$1
 
+[[ ! -d "downloads" ]] && mkdir "downloads"
+[[ ! -d "downloads/rec_test" ]] && mkdir "downloads/rec_test"
 
-[[ ! -d "gmts" ]] && mkdir "gmts"
-
-#echo $URL_PARSE                # Just prints the array
-
-for i in ${URL_PARSE[@]}
+for i in ${EXT_PARSE[@]}
 do
-    $(wget "$i" -P gmts/) 
+    case ARG in
+        '-R')    
+            for i in ${HTML_PARSE[@]}
+            do
+                $(wget "$i" -P downloads/rec_test/) 
+            done
+            ;;  
+        '-h')
+            echo "./gmts\t.......\tGive Me That Shit\n\t-h:\n\tHelp page"; exit
+            ;;
+    esac
+    $(wget "$i" -c -q -P downloads/)
 done
-
